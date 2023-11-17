7aed64497d583e73e4d5435edf2a445fe3fea305
abhishekjadhav3470 <abhishekjadhav3470@gmail.com>
2023-11-17 20:08:19 +0530

    Executed the packages and modules

--- modules and packages/payment/payment_details.py
+++ modules and packages/payment/payment_details.py
@@ -1,2 +1,13 @@
+import os, sys 
+from os.path import dirname, join, abspath
+
+
+sys.path.insert(0, abspath(join(dirname(__file__), "..")))
+# the above is written to clear all the path to the proper file for the function
+
+from course import course_details
+
 def payment():
     print("this is my payment file")
+
+course_details.course()
